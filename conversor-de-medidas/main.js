8f74dfe5d0abb8099924b1af0226a3a58d97d605
Arthur Quaresma <101885609+pendragonarthur@users.noreply.github.com>
2022-09-05 17:47:42 -0300

    Add files via upload

---
+++ conversor-de-medidas/main.js
@@ -0,0 +1,35 @@
+// Exercício 3 - "Conversor de medidas";
+
+let medidaInicial = Number(prompt("Insira a medida em metros."))
+let menu = prompt("Para qual medida deseja converter? \n a) Milímetros \n b) Decímetros \n c) Centímetros \n d) Decâmetros \n e) Hectômetros \n f) Quilômetros")
+let medidaFinal; 
+const conversor = { 
+    a: function mm(){ return medidaFinal = medidaInicial*1000},
+    b: function dm(){ return medidaFinal = medidaInicial*100},
+    c: function cm(){ return medidaFinal = medidaInicial*10},
+    d: function dam(){ return medidaFinal = medidaInicial/10},
+    e: function hec(){ return medidaFinal = medidaInicial/100},
+    f: function km(){ return medidaFinal = medidaInicial/1000}
+}
+
+switch(menu){ 
+    case 'a': 
+    alert("A medida agora é: " + conversor.a())
+    break
+    case 'b': 
+    alert("A medida agora é: " + conversor.b())
+    break
+    case 'c': 
+    alert("A medida agora é: " + conversor.c())
+    break
+    case 'd': 
+    alert("A medida agora é: " + conversor.d())
+    break
+    case 'e': 
+    alert("A medida agora é: " + conversor.e())
+    break
+    case 'f': 
+    alert("A medida agora é: " + conversor.f())
+    break
+    default: alert("Inserção inválida.")
+}
