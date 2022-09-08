87abc34967345221b71d0dbd31ca3f7ad42c026b
Arthur Quaresma <101885609+pendragonarthur@users.noreply.github.com>
2022-09-08 16:43:54 -0300

    Update main.js

--- calculadora/main.js
+++ calculadora/main.js
@@ -1,4 +1,6 @@
-//Exercício 2 - "Calculadora 4 operações";
+// Exercício 2 - "Calculadora 4 operações";
+// Exercício para fixação de operações matemáticas básicas. Armazenei as operações em uma constante afim de exercitar boas práticas como diminuição do uso do IF() e
+// utilizei um switch para chamar as funções de cada operação.
 
 let a = Number(prompt("Insira um número."))
 let b = Number(prompt("Insira o segundo número."))
