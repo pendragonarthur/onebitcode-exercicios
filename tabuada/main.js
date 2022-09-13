45ea53731c0dcf723d4657ebf7166addc7e81842
Arthur Quaresma <101885609+pendragonarthur@users.noreply.github.com>
2022-09-13 17:59:36 -0300

    Create main.js

---
+++ tabuada/main.js
@@ -0,0 +1,8 @@
+// Exercício 7 - "Robô da tabuada"
+
+let number = Number(prompt("Que número deseja realizar a tabuada?"))
+
+for(let i = 1; i <= 20; i++){ 
+    let x = number*i
+    alert(number +"x"+ i +"="+ x)
+}
